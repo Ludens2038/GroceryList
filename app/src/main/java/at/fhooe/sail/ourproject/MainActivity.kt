8000eb152333267187e285a19b2a0b2b7d82dd02
Haris <s2310237010@students.fh-hagenberg.at>
2024-05-30 16:06:32 +0200

    main activity created

--- app/src/main/java/at/fhooe/sail/ourproject/MainActivity.kt
+++ app/src/main/java/at/fhooe/sail/ourproject/MainActivity.kt
@@ -5,10 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import at.fhooe.sail.ourproject.databinding.ActivityMainBinding
+
+const val TAG: String = "ListTest"
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
